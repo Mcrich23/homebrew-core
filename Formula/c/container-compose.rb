008c601b0c8feb89dc360b5aff94d8e914727a8f
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-11-20 10:51:28 -0800

    Container-Compose version updated to 0.6.1

--- Formula/c/container-compose.rb
+++ Formula/c/container-compose.rb
@@ -1,13 +1,13 @@
 class ContainerCompose < Formula
   desc "Manage Apple Container with Docker Compose files"
   homepage "https://github.com/mcrich23/container-compose"
-  url "https://github.com/Mcrich23/container-compose/archive/refs/tags/0.6.0.tar.gz"
-  sha256 "6deefee705521b5e388ebf9df1280d56a1bcf0f3900f70fbe7b4ddfc9e317b48"
+  url "https://github.com/Mcrich23/container-compose/archive/refs/tags/0.6.1.tar.gz"
+  sha256 "2902a747fe1205adbb1bc36ab58788291cf47082deb978cf6e1343c7daf2d7a7"
   license "MIT"
   head "https://github.com/mcrich23/container-compose.git", branch: "main"
 
   bottle do
-    sha256 arm64_tahoe: "32878f76c1de9507cfddb8da3afc9d14e0f84f1dbd71b74d686cf1cb7d478157"
+    sha256 arm64_tahoe: "2902a747fe1205adbb1bc36ab58788291cf47082deb978cf6e1343c7daf2d7a7"
   end
 
   depends_on xcode: ["26.0", :build]
